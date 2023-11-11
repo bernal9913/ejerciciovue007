dfc7a40713d151bf2deeb9bdd3d553c8ae5186ad
bernal9913 <carlosbernal@Carloss-MacBook-Air.local>
2023-11-11 11:33:20 -0700

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h1>Mi Lista de Compras</h1>
+    <shopping-list :shopping-items="items" />
+  </div>
+</template>
+
+<script>
+import ShoppingList from './components/ShoppingList.vue';
+
+export default {
+  data() {
+    return {
+      items: ['Nutella', 'Huevos kinder', 'MAndarinas', 'un seisillo'],
+    };
+  },
+  components: {
+    ShoppingList,
+  },
+};
+</script>
---
+++ src/components/ShoppingList.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h2>Lista de Compras</h2>
+    <ul>
+      <li v-for="item in shoppingItems" :key="item">
+        {{ item }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    shoppingItems: {
+      type: Array,
+      required: true,
+    },
+  },
+};
+</script>
